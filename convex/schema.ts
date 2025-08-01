70588209d6524bab2e6a45f8e5d3caeeafccd72d
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2025-08-01 22:30:21 +0530

    schema defined

---
+++ convex/schema.ts
@@ -0,0 +1,46 @@
+import { defineTable, defineSchema } from "convex/server";
+
+import { v } from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        userId: v.string(),  // This is the id by clerk
+        email: v.string(),
+        name: v.string(),
+        isPro: v.boolean(),
+        proSince: v.optional(v.number()),
+        stripCustomerId: v.optional(v.string()),
+        stripOrderId: v.optional(v.string()),
+    }).index("by_user_id", ["userId"]),
+
+    codeExecutions: defineTable({
+        userId: v.string(),
+        language: v.string(),
+        code: v.string(),
+        output: v.optional(v.string()),
+        error: v.optional(v.string()),
+    }).index("by_user_id", ["userId"]),
+
+    snippets: defineTable({
+        userId: v.string(),
+        title: v.string(),
+        language: v.string(),
+        code: v.string(),
+        userName: v.string(),   // store user's name for the particular snippet
+    }).index("by_user_id", ["userId"]),
+
+    snippetComments: defineTable({
+        snippetId: v.id("snippets"),
+        userId: v.string(),
+        userName: v.string(),
+        content: v.string(),
+    }).index("by_snippet_id", ["snippetId"]),
+
+    stars: defineTable({
+        userId: v.string(),
+        snippetId: v.id("snippets"),
+    })
+       .index("by_user_id", ["userId"])
+       .index("by_snippet_id", ["snippetId"])
+       .index("by_user_id_and_snippet_id", ["userId", "snippetId"]),
+})
